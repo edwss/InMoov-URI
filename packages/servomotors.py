69c18dbfadfc962209e10cc9b99fa58d904e4327
Gustavo Teixeira <iwhrim@gmail.com>
2018-09-26 05:55:47 -0300

    Servomotor and speechrecognition package creation

--- packages/servomotors.py
+++ packages/servomotors.py
@@ -33,39 +33,39 @@ class Servomotors:
     horizontal_head_MIDDLE_ANGLE = 0
     horizontal_head_MAX_ANGLE = 0
 
-     def angleToPulse(self, angle):
-         pulse = (angle - 0) * (servo_max - servo_min) / (180 - 0) + servo_min
-         return pulse
+    def angleToPulse(self, angle):
+        pulse = (angle - 0) * (servo_max - servo_min) / (180 - 0) + servo_min
+        return pulse
 
     def __init__(self):
         super().__init__()
         pwm = Adafruit_PCA9685.PCA9685()
         pwm.set_pwm_freq(60)
-        self.neutral()
+        # self.neutral()
 
     # Here I'll have to set all servomotors to middle angle.
     # This should be used when starting the InMoov and before turning it off
-    def neutral(self):
-
-    def headUp(self, angle):
-
-    def headDown(self, angle):
-
-    def headLeft(self, angle):
-
-    def headRight(self, angle):
-
-    def eyesUp(self, angle):
-
-    def eyesDown(self, angle):
-
-    def eyesLeft(self, angle):
-
-    def eyesRight(self, angle):
-
-    def closeMouth(self):
-
-    def mouthAngle(self, angle):
+    # def neutral(self):
+    #
+    # def headUp(self, angle):
+    #
+    # def headDown(self, angle):
+    #
+    # def headLeft(self, angle):
+    #
+    # def headRight(self, angle):
+    #
+    # def eyesUp(self, angle):
+    #
+    # def eyesDown(self, angle):
+    #
+    # def eyesLeft(self, angle):
+    #
+    # def eyesRight(self, angle):
+    #
+    # def closeMouth(self):
+    #
+    # def mouthAngle(self, angle):
 
 
 
