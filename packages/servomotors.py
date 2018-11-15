927a39682938ca8ec6c2bd2c3c52a47bf112b2eb
Gustavo Teixeira <iwhrim@gmail.com>
2018-11-15 04:28:02 -0200

    Adjustments in some functions

--- packages/servomotors.py
+++ packages/servomotors.py
@@ -1,15 +1,16 @@
 from __future__ import division
 import time
 import Adafruit_PCA9685
-#from time import sleep
+
+
+# from time import sleep
 
 class Servomotors:
-
     servo_min = 150  # Min pulse length out of 4096
     servo_max = 600  # Max pulse length out of 4096
 
     mouth_GPIO_PIN = 0
-    mouth_MIN_ANGLE = 105
+    mouth_MIN_ANGLE = 100
     mouth_MIDDLE_ANGLE = 125
     mouth_MAX_ANGLE = 150
 
@@ -29,8 +30,8 @@ class Servomotors:
     head_vertical_MAX_ANGLE = 140
 
     head_horizontal_GPIO_PIN = 4
-    head_horizontal_MIN_ANGLE = 25
-    head_horizontal_MIDDLE_ANGLE = 88
+    head_horizontal_MIN_ANGLE = 40
+    head_horizontal_MIDDLE_ANGLE = 85
     head_horizontal_MAX_ANGLE = 130
 
     def angleToPulse(self, angle):
@@ -46,24 +47,24 @@ class Servomotors:
     def neutral(self):
         pwm = Adafruit_PCA9685.PCA9685()
         pwm.set_pwm_freq(60)
-        pwm.set_pwm(0, 0, self.angleToPulse(self.head_vertical_MIDDLE_ANGLE)) #Head vertical
-        pwm.set_pwm(0, 0, self.angleToPulse(self.head_horizontal_MIDDLE_ANGLE)) #Head horizontal
-        pwm.set_pwm(0, 0, self.angleToPulse(self.eye_vertical_MIDDLE_ANGLE)) #Eye vertical
-        pwm.set_pwm(0, 0, self.angleToPulse(self.eye_horizontal_MIDDLE_ANGLE)) #Eye horizontal
-        pwm.set_pwm(0, 0, self.angleToPulse(self.mouth_MIDDLE_ANGLE)) #Mouth
+        pwm.set_pwm(0, 0, self.angleToPulse(self.head_vertical_MIDDLE_ANGLE))  # Head vertical
+        pwm.set_pwm(0, 0, self.angleToPulse(self.head_horizontal_MIDDLE_ANGLE))  # Head horizontal
+        pwm.set_pwm(0, 0, self.angleToPulse(self.eye_vertical_MIDDLE_ANGLE))  # Eye vertical
+        pwm.set_pwm(0, 0, self.angleToPulse(self.eye_horizontal_MIDDLE_ANGLE))  # Eye horizontal
+        pwm.set_pwm(0, 0, self.angleToPulse(self.mouth_MIDDLE_ANGLE))  # Mouth
 
     def moveTo(self, servomotor, angle):
         pwm = Adafruit_PCA9685.PCA9685()
         pwm.set_pwm_freq(60)
         pwm.set_pwm(servomotor, 0, self.angleToPulse(angle))
 
-#THESE FUNCTIONS NEED A SPEED PARAMETER!!!!!!!!!1
+    # THESE FUNCTIONS NEED A SPEED PARAMETER!!!!!!!!!1
 
     def headUp(self, speed):
-        for angle in range(self.head_vertical_MIDDLE_ANGLE, self.head_vertical_MAX_ANGLE):
-            self.moveTo(self.head_vertical_GPIO_PIN, angle)
-        #delay
-        #self.moveTo(self.head_vertical_GPIO_PIN, self.head_vertical_MAX_ANGLE)
+        self.moveTo(self.head_vertical_GPIO_PIN, self.head_vertical_MAX_ANGLE)
+        # for angle in range(self.head_vertical_MIDDLE_ANGLE, self.head_vertical_MAX_ANGLE):
+        #    self.moveTo(self.head_vertical_GPIO_PIN, angle)
+        # delay
 
     def headDown(self, speed):
         self.moveTo(self.head_vertical_GPIO_PIN, self.head_vertical_MIN_ANGLE)
@@ -71,47 +72,56 @@ class Servomotors:
     def headLeft(self, speed):
         self.moveTo(self.head_horizontal_GPIO_PIN, self.head_horizontal_MAX_ANGLE)
 
-    # def headRight(self, angle):
+    def headRight(self, speed):
+        self.moveTo(self.head_horizontal_GPIO_PIN, self.head_horizontal_MIN_ANGLE)
 
-    # def eyesUp(self, angle):
-    #
-    # def eyesDown(self, angle):
-    #
-    # def eyesLeft(self, angle):
-    #
-    # def eyesRight(self, angle):
-    #
-    # def mouthClose(self):
-    #
-    # def mouthOpen(self, angle):
-    #
-    # def headHorizontalTo(self,angle):
-    #
-    # def headVerticalTo(self, angle):
-    #
-    # def eyeHorizontalTo(self,angle):
-    #
-    # def eyeVerticalTo(self, angle):
-    #
-    # def mouthTo(self,angle):
-    #
-    # def headTo2D(self, xCoord, yCoord):
-    #
-    # def eyesTo2D(self, xCoord, yCoord):
+    def eyesUp(self, speed):
+        self.moveTo(self.eye_vertical_GPIO_PIN, self.eye_vertical_MIN_ANGLE)
 
+    def eyesDown(self, speed):
+        self.moveTo(self.eye_vertical_GPIO_PIN, self.head_vertical_MAX_ANGLE)
 
-#ANIMAÇÕES
+    def eyesLeft(self, speed):
+        self.moveTo(self.eye_horizontal_GPIO_PIN, self.eye_horizontal_MAX_ANGLE)
 
-#SIM
+    def eyesRight(self, speed):
+        self.moveTo(self.eye_horizontal_GPIO_PIN, self.eye_horizontal_MIN_ANGLE)
 
-#NÃO
+    def mouthClose(self, speed):
+        self.moveTo(self.mouth_GPIO_PIN, self.mouth_MIN_ANGLE)
 
-#TALVEZ
+    def mouthOpen(self, speed):
+        self.moveTo(self.mouth_GPIO_PIN, self.mouth_MAX_ANGLE)
 
-#PESQUISAR MAIS ANIMAÇÕES SIMPLES DE CABEÇAS ROBÓTICAS HUMANOIDES
+    def headHorizontalTo(self, speed, angle):
+        self.moveTo(self.head_horizontal_GPIO_PIN, angle)
 
+    def headVerticalTo(self, speed, angle):
+        self.moveTo(self.head_vertical_GPIO_PIN, angle)
 
+    def eyeHorizontalTo(self, speed, angle):
+        self.moveTo(self.eye_horizontal_GPIO_PIN, angle)
 
+    def eyeVerticalTo(self, speed, angle):
+        self.moveTo(self.eye_vertical_GPIO_PIN, angle)
 
+    def mouthTo(self,speed, angle):
+        self.moveTo(self.mouth_GPIO_PIN, angle)
 
+    def headTo2D(self, xCoord, yCoord):
+        self.headHorizontalTo(10, xCoord)
+        self.headVerticalTo(10, yCoord)
 
+    def eyesTo2D(self, xCoord, yCoord):
+        self.eyeHorizontalTo(10, xCoord)
+        self.eyeVerticalTo(10, yCoord)
+
+# ANIMAÇÕES
+
+# SIM
+
+# NÃO
+
+# TALVEZ
+
+# PESQUISAR MAIS ANIMAÇÕES SIMPLES DE CABEÇAS ROBÓTICAS HUMANOIDES
